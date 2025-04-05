1c6f190bbbb297c99df9a9c0a04ddf1ba4bcbdb6
WhosMadeer <mahirkhandaker@gmail.com>
2025-04-05 13:24:19 -0400

    disabled supply

--- M3/LimiterTestScript.py
+++ M3/LimiterTestScript.py
@@ -24,10 +24,10 @@ def end_program():
     scope.write(":WGEN:OUTP OFF")
     fxngen.write("OUTPut1 OFF")
     fxngen.write("OUTPut2 OFF")
-    supply.write("OUTP 0, (@2)")
+    # supply.write("OUTP 0, (@2)")
     scope.close()
     fxngen.close()
-    supply.close()
+    # supply.close()
     print("closed connection")
     exit()
 
@@ -38,18 +38,18 @@ print(rm)
 print(rm.list_resources("TCPIP0::?*"))
 
 """Connect to the different devices"""
-supply = rm.open_resource("TCPIP0::192.168.0.251::5025::SOCKET")
+# supply = rm.open_resource("TCPIP0::192.168.0.251::5025::SOCKET")
 scope = rm.open_resource("TCPIP0::192.168.0.253::hislip0::INSTR")
 fxngen = rm.open_resource("TCPIP0::192.168.0.254::5025::SOCKET")
 
 """ Set up the IO configuration"""
-supply.timeout = 10000  # 10s
+# supply.timeout = 10000  # 10s
 scope.timeout = 10000  # 10s
 fxngen.timeout = 10000  # 10s
 
 # Define string terminations
-supply.write_termination = "\n"
-supply.read_termination = "\n"
+# supply.write_termination = "\n"
+# supply.read_termination = "\n"
 scope.write_termination = "\n"
 scope.read_termination = "\n"
 fxngen.write_termination = "\n"
@@ -57,7 +57,7 @@ fxngen.read_termination = "\n"
 
 
 # Get ID info
-print("supply ID string:\n  ", supply.query("*IDN?"), flush=True)
+# print("supply ID string:\n  ", supply.query("*IDN?"), flush=True)
 print("Connected to oscilloscope:", scope.query("*IDN?"), flush=True)
 print("Connected to function generator:", fxngen.query("*IDN?"), flush=True)
 
